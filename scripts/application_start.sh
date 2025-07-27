643bc2cc2745f46f47325047a3280e26313eb1a6
Rahul Kumar <Rahul.Kumar@quantum-health.com>
2025-07-27 19:07:23 +0530

    Fix app_start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run application_start.sh: ' >> /home/ec2-user/react-app-pipeline/deploy.log
-echo 'cd /home/ec2-user/react-app-pipeline' >> /home/ec2-user/react-app-pipeline/deploy.log
-cd /home/ec2-user/react-app-pipeline >> /home/ec2-user/react-app-pipeline/deploy.log
-echo 'npm run build' >> /home/ec2-user/react-app-pipeline/deploy.log
-npm run build >> /home/ec2-user/react-app-pipeline/deploy.log 2>&1
-echo 'starting react app with serve' >> /home/ec2-user/react-app-pipeline/deploy.log
-pm2 start serve --name "react-app" -- -s build -l 3000 >> /home/ec2-user/react-app-pipeline/deploy.log 2>&1
+echo 'run application_start.sh: ' >> /home/ubuntu/react-app-pipeline/deploy.log
+echo 'cd /home/ubuntu/react-app-pipeline' >> /home/ubuntu/react-app-pipeline/deploy.log
+cd /home/ubuntu/react-app-pipeline >> /home/ubuntu/react-app-pipeline/deploy.log
+echo 'npm run build' >> /home/ubuntu/react-app-pipeline/deploy.log
+npm run build >> /home/ubuntu/react-app-pipeline/deploy.log 2>&1
+echo 'starting react app with serve' >> /home/ubuntu/react-app-pipeline/deploy.log
+pm2 start serve --name "react-app" -- -s build -l 3000 >> /home/ubuntu/react-app-pipeline/deploy.log 2>&1
